ab5c8a25e3ee2b93fd0b9b3d0225f86eeb643f87
GitHub Action <action@github.com>
2024-07-02 01:01:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/sydowma/fastplugin/MyBundle.kt
+++ src/main/kotlin/com/github/sydowma/fastplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.sydowma.fastplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/sydowma/fastplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/sydowma/fastplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.sydowma.fastplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sydowma.fastplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
